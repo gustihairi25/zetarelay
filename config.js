3fdc7e09f9f112c47804917fc4bd90bc9296d59a
gustihairi25 <gustihairi25@gmail.com>
2026-01-29 09:26:19 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
 const APP_CONFIG = {
-    // 10 Alamat Dompet Crypto Anda (Token Publik)
-    depositAddresses: {
-        "BTC": "bc1q6al6pkdkwt7vpgujwr7fvnjxvkdaet64q5rvnj",
-        "ETH": "0x4c51175cdf9b6311ce152694f146d29be8911432",
-        "SOL": "5iShtcHj9sAGXoRwbCb5f51ew9B3Rjtn9yM5CXABLgi2",
-        "TRX": "TBgjd7vYBzFzoUkhtmYtFQvTxhdjcF36E2",
-        "BNB": "0x4c51175cdf9b6311ce152694f146d29be8911432",
-        "MATIC": "0x4c51175cdf9b6311ce152694f146d29be8911432",
-        "ARB": "0x4c51175cdf9b6311ce152694f146d29be8911432",
-        "BASE": "0x4c51175cdf9b6311ce152694f146d29be8911432",
-        "OP": "0x4c51175cdf9b6311ce152694f146d29be8911432",
-        "LINEA": "0x4c51175cdf9b6311ce152694f146d29be8911432"
+    projectId: 'Cec254c96efb6947d34d6979423c88ce', 
+    adminWallet: '0x4c51175cdf9b6311ce152694f146d29be8911432', 
+    serviceFee: 0.01, 
+    network: {
+        chainId: 7000,
+        rpcUrl: 'https://zetachain-mainnet-archive.allthatnode.com:8545'
     },
-
-    // Konfigurasi Firebase Anda (Sesuai Chat Sebelumnya)
     firebase: {
         apiKey: "AIzaSyDrpBJTtankyLgimtOwuriFpiyuC5K10OI",
         authDomain: "studio-727777898-40aea.firebaseapp.com",
@@ -21,6 +13,7 @@ const APP_CONFIG = {
         projectId: "studio-727777898-40aea",
         storageBucket: "studio-727777898-40aea.firebasestorage.app",
         messagingSenderId: "351263967108",
-        appId: "1:351263967108:web:a0c01af41422903cc17fd1"
+        appId: "1:351263967108:web:973eb49037d874e5c17fd1",
+        measurementId: "G-C1LTN3E50Q"
     }
 };
